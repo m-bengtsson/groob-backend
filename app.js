4e3bebe8f5a23bb38a9fa4e51c1b00366edf458e
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-10-13 14:03:59 +0200

    Added number_of_items

--- app.js
+++ app.js
@@ -6,24 +6,24 @@ const app = express();
 app.use(express.json());
 
 app.get("/items", async (req, res) => {
-	const items = await getItems();
-	res.send(items);
+   const items = await getItems();
+   res.send(items);
 });
 
 app.get("/items/:id", async (req, res) => {
-	const id = req.params.id;
-	const item = await getItem(id);
-	res.send(item);
+   const id = req.params.id;
+   const item = await getItem(id);
+   res.send(item);
 });
 
 app.post("/items", async (req, res) => {
-	console.log("BODY", req.body);
-	const { title, body, created_by } = req.body;
-	const created = await createItem(title, body, created_by);
+   console.log("BODY", req.body);
+   const { title, body, created_by, number_of_items } = req.body;
+   const created = await createItem(title, body, created_by, number_of_items);
 
-	res.status(201).send(created);
+   res.status(201).send(created);
 });
 
 app.listen(8080, () => {
-	console.log("Server running on port 8080");
+   console.log("Server running on port 8080");
 });
