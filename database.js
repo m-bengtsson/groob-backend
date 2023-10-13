8d3482b63d20b3649a9394aa8a9a1d0c04752adc
Kate <kate.nordqvist@gmail.com>
2023-10-13 21:04:13 +0200

    add item_id to createItem
    make relatively unique id with Date.now()
    add created id to createItems

--- database.js
+++ database.js
@@ -4,17 +4,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql
-   .createPool({
-      host: process.env.MYSQL_HOST,
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
-   })
-   .promise();
+	.createPool({
+		host: process.env.MYSQL_HOST,
+		user: process.env.MYSQL_USER,
+		password: process.env.MYSQL_PASSWORD,
+		database: process.env.MYSQL_DATABASE,
+	})
+	.promise();
 
 export const getItems = async () => {
-   const [rows] = await pool.query("SELECT * from items");
-   return rows;
+	const [rows] = await pool.query("SELECT * from items");
+	return rows;
 };
 
 //Using ? instead of value directly, the id is an untrusted value and we don't want it to be part of the query.
@@ -22,18 +22,20 @@ export const getItems = async () => {
 //Otherwise it could lead to SQL injection attacks
 // syntax is called prepared statement
 export const getItem = async (id) => {
-   const [rows] = await pool.query("SELECT * FROM items WHERE id = ?", [id]);
-   return rows[0]; //Want the first object in the array
-   //returns undefined if the id does not exist
+	const [rows] = await pool.query("SELECT * FROM items WHERE id = ?", [id]);
+	return rows[0]; //Want the first object in the array
+	//returns undefined if the id does not exist
 };
 
 export const createItem = async (title, body, created_by, number_of_items) => {
-   const [result] = await pool.query(
-      "INSERT INTO items (title, body, created_by, number_of_items) VALUES (?, ?, ?, ?)",
-      [title, body, created_by, number_of_items]
-   );
-   const id = result.insertId;
+	//get relatively unique number
+	const item_id = Date.now();
+	const [result] = await pool.query(
+		"INSERT INTO items (item_id, title, body, created_by, number_of_items) VALUES (?, ?, ?, ?)",
+		[item_id, title, body, created_by, number_of_items]
+	);
+	const id = result.insertId;
 
-   //Get the newly created item
-   return getItem(id);
+	//Get the newly created item
+	return getItem(id);
 };
