380b0b0c6345a767562ea97120cade92b7d50b36
Kate <kate.nordqvist@gmail.com>
2023-10-13 21:46:38 +0200

    add app.delete items route
    get id from req.params
    call deleteItem(id)
    sendstatus 400 if item is 0
    else sendstatus 200

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
 import express from "express";
-import { getItems, getItem, createItem } from "./database.js";
+import { getItems, getItem, createItem, deleteItem } from "./database.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/items", async (req, res) => {
-   const items = await getItems();
-   res.send(items);
+	const items = await getItems();
+	res.send(items);
 });
 
 app.get("/items/:id", async (req, res) => {
-   const id = req.params.id;
-   const item = await getItem(id);
-   res.send(item);
+	const id = req.params.id;
+	const item = await getItem(id);
+	res.send(item);
 });
 
 app.post("/items", async (req, res) => {
-   console.log("BODY", req.body);
-   const { title, body, created_by, number_of_items } = req.body;
-   const created = await createItem(title, body, created_by, number_of_items);
+	console.log("BODY", req.body);
+	const { title, body, created_by, number_of_items } = req.body;
+	const created = await createItem(title, body, created_by, number_of_items);
 
-   res.status(201).send(created);
+	res.status(201).send(created);
+});
+
+app.delete("/items/:id", async (req, res) => {
+	const id = req.params.id;
+	const item = await deleteItem(id);
+
+	//result.affectedRows will be 0 if item is not found
+	if (item === 0) {
+		res.sendStatus(400);
+	}
+
+	res.sendStatus(200);
 });
 
 app.listen(8080, () => {
-   console.log("Server running on port 8080");
+	console.log("Server running on port 8080");
 });
