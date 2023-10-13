186f7f4744fc5782d1db5ccb0cb6b8526033cd06
Kate <kate.nordqvist@gmail.com>
2023-10-13 09:58:00 +0200

    setup connection to mysql

---
+++ database.js
@@ -0,0 +1,47 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = mysql
+	.createPool({
+		host: process.env.MYSQL_HOST,
+		user: process.env.MYSQL_USER,
+		password: process.env.MYSQL_PASSWORD,
+		database: process.env.MYSQL_DATABASE,
+	})
+	.promise();
+
+export const getItems = async () => {
+	const [rows] = await pool.query("SELECT * from items");
+	return rows;
+};
+
+//Using ? instead of value directly, the id is an untrusted value and we don't want it to be part of the query.
+//Send id seperately as an array
+//Otherwise it could lead to SQL injection attacks
+// sytax is called prepared statement
+export const getItem = async (id) => {
+	const [rows] = await pool.query("SELECT * FROM items WHERE id = ? ", [id]);
+	return rows[0]; //Want the first object in the array
+	//returns undefined if the id does not exist
+};
+
+export const createItem = async (title, body, created_by) => {
+	const [result] = await pool.query(
+		"INSERT INTO items (title, body, created_by) VALUES (?, ?, ?)",
+		[title, body, created_by]
+	);
+	const id = result.insertId;
+
+	//Get the newly created item
+	return getItem(id);
+};
+
+/* const items = await getItems();
+const item = await getItem(2);
+const create = await createItem("socks", "This is a sock", 3);
+
+console.log("ITEMS", items);
+console.log("ITEM", item);
+console.log("CREATED", create); */
