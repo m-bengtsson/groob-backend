3624f2425b08fd90ab26937c1287197830df1128
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-10-16 14:40:12 +0200

    get user/:id api

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {
-	getItems,
-	getItem,
-	createItem,
-	deleteItem,
-	getUsers,
+   getItems,
+   getItem,
+   createItem,
+   deleteItem,
+   getUsers,
+   getUser
 } from "./database.js";
 
 const app = express();
@@ -12,41 +13,47 @@ const app = express();
 app.use(express.json());
 
 app.get("/items", async (req, res) => {
-	const items = await getItems();
-	res.send(items);
+   const items = await getItems();
+   res.send(items);
 });
 
 app.get("/items/:id", async (req, res) => {
-	const id = req.params.id;
-	const item = await getItem(id);
-	res.send(item);
+   const id = req.params.id;
+   const item = await getItem(id);
+   res.send(item);
 });
 
 app.post("/items", async (req, res) => {
-	console.log("BODY", req.body);
-	const { title, body, created_by, number_of_items } = req.body;
-	const created = await createItem(title, body, created_by, number_of_items);
+   console.log("BODY", req.body);
+   const { title, body, created_by, number_of_items } = req.body;
+   const created = await createItem(title, body, created_by, number_of_items);
 
-	res.status(201).send(created);
+   res.status(201).send(created);
 });
 
 app.delete("/items/:id", async (req, res) => {
-	const id = req.params.id;
-	const item = await deleteItem(id);
+   const id = req.params.id;
+   const item = await deleteItem(id);
 
-	//result.affectedRows will be 0 if item is not found
-	if (item === 0) {
-		res.sendStatus(400);
-	}
+   //result.affectedRows will be 0 if item is not found
+   if (item === 0) {
+      res.sendStatus(400);
+   }
 
-	res.sendStatus(200);
+   res.sendStatus(200);
 });
 
 app.get("/users", async (req, res) => {
-	const users = await getUsers();
-	res.send(users);
+   const users = await getUsers();
+   res.send(users);
+});
+
+app.get("/users/:id", async (req, res) => {
+   const id = req.params.id;
+   const user = await getUser(id);
+   res.send(user);
 });
 
 app.listen(8080, () => {
-	console.log("Server running on port 8080");
+   console.log("Server running on port 8080");
 });
