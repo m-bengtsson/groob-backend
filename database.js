9d542ca6ed623749f74dc9da16b1e8360c8449b2
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-10-16 11:00:01 +0200

    get one user db
    get one user from the database and change "rows" to "result"

--- database.js
+++ database.js
@@ -4,17 +4,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql
-	.createPool({
-		host: process.env.MYSQL_HOST,
-		user: process.env.MYSQL_USER,
-		password: process.env.MYSQL_PASSWORD,
-		database: process.env.MYSQL_DATABASE,
-	})
-	.promise();
+   .createPool({
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+   })
+   .promise();
 
 export const getItems = async () => {
-	const [rows] = await pool.query("SELECT * from items");
-	return rows;
+   const [result] = await pool.query("SELECT * from items");
+   return result;
 };
 
 //Using ? instead of value directly, the id is an untrusted value and we don't want it to be part of the query.
@@ -22,37 +22,41 @@ export const getItems = async () => {
 //Otherwise it could lead to SQL injection attacks
 // syntax is called prepared statement
 export const getItem = async (id) => {
-	const [rows] = await pool.query("SELECT * FROM items WHERE item_id = ?", [
-		id,
-	]);
-	return rows[0]; //Want the first object in the array
-	//returns undefined if the id does not exist
+   const [result] = await pool.query("SELECT * FROM items WHERE item_id = ?", [
+      id,
+   ]);
+   return result[0]; //Want the first object in the array
+   //returns undefined if the id does not exist
 };
 
 export const createItem = async (title, body, created_by, number_of_items) => {
-	//change item_id later
-	const item_id = Math.floor(Math.random() * 10000);
-	console.log("ID", item_id);
-	const [result] = await pool.query(
-		"INSERT INTO items (item_id, title, body, created_by, number_of_items) VALUES (?, ?, ?, ?, ?)",
-		[item_id, title, body, created_by, number_of_items]
-	);
-	const id = result.insertId;
+   //change item_id later
+   const item_id = Math.floor(Math.random() * 10000);
+   const [result] = await pool.query(
+      "INSERT INTO items (item_id, title, body, created_by, number_of_items) VALUES (?, ?, ?, ?, ?)",
+      [item_id, title, body, created_by, number_of_items]
+   );
+   const id = result.insertId;
 
-	//Get the newly created item
-	return getItem(id);
+   //Get the newly created item
+   return getItem(id);
 };
 
 export const deleteItem = async (id) => {
-	const [result] = await pool.query("DELETE FROM items WHERE item_id = ?", [
-		id,
-	]);
+   const [result] = await pool.query("DELETE FROM items WHERE item_id = ?", [
+      id,
+   ]);
 
-	//result.affectedRows will be 0 if item is not found
-	return result.affectedRows;
+   //result.affectedRows will be 0 if item is not found
+   return result.affectedRows;
 };
 
 export const getUsers = async () => {
-	const [result] = await pool.query("SELECT * from users");
-	return result;
+   const [result] = await pool.query("SELECT * from users");
+   return result;
+};
+
+export const getUser = async (id) => {
+   const [result] = await pool.query("SELECT * from users WHERE uid = ?", [id]);
+   return result[0];
 };
