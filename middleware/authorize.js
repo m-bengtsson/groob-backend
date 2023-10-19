f2b847aa10fd44b16533ba107357fceec7ac3502
Kate <kate.nordqvist@gmail.com>
2023-10-19 15:29:35 +0200

    middleware authorize
    check if user has admin role
    use middleware on selected routes

---
+++ middleware/authorize.js
@@ -0,0 +1,12 @@
+import { getUser } from "../database.js";
+
+export const isAdmin = async (req, res, next) => {
+	const { uid } = req.body;
+	const user = await getUser(uid);
+	if (user.roles === "admin") {
+		req.user = user;
+		next();
+	} else {
+		return res.status(403).send("Neeej, vad synd, du är inte behörig");
+	}
+};
