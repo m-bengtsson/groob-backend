f7fb62eff155a2e33b46d8d870a57d6fc7a0b24e
Kate <kate.nordqvist@gmail.com>
2023-10-19 14:59:47 +0200

    send user instead of email

--- routes/identity.js
+++ routes/identity.js
@@ -1,46 +1,50 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import { getUserByEmail } from "../database.js";
+import { validateLogin } from "../middleware/validate.js";
 
-const router = express.Router()
+const router = express.Router();
 dotenv.config();
 
 const secret_key = process.env.SECRET_KEY;
 
-router.post("/login", async (req, res) => {
-   const { email, password } = req.body;
-   const user = await getUserByEmail(email);
+router.post("/login", validateLogin, async (req, res) => {
+	const { email, name, uid } = req.user;
 
-   if (user.password === password) {
-      const accessToken = jwt.sign({ email }, secret_key, { expiresIn: "15m" });
-      const refreshToken = jwt.sign({ email }, secret_key, { expiresIn: "1d" });
+	const user = { email, name, uid };
 
-      res
-         .cookie("refreshToken", refreshToken, {
-            httpOnly: true,
-            sameSite: "strict",
-         })
-         .header("Authorization", accessToken)
-         .send(email);
-   }
+	const accessToken = jwt.sign({ user }, secret_key, {
+		expiresIn: "15m",
+	});
+	const refreshToken = jwt.sign({ user }, secret_key, {
+		expiresIn: "1d",
+	});
+
+	res
+		.cookie("refreshToken", refreshToken, {
+			httpOnly: true,
+			sameSite: "strict",
+		})
+		.header("Authorization", accessToken)
+		.send(user);
 });
 
 router.post("/refresh", async (req, res) => {
-   const refreshToken = req.cookies["refreshToken"];
+	const refreshToken = req.cookies["refreshToken"];
 
-   if (!refreshToken) {
-      return res.status(401).send("Neej va synd, du hade ingen refreshtoken :/");
-   }
-   try {
-      const decoded = jwt.verify(refreshToken, secret_key);
-      const accessToken = jwt.sign({ email: decoded.email }, secret_key, {
-         expiresIn: "15m",
-      });
-      res.status(200).header("Authorization", accessToken).send(decoded.email);
-   } catch (error) {
-      res.status(401).send("Neej va synd du får ingen ny token :'(");
-   }
+	if (!refreshToken) {
+		return res.status(401).send("Neej va synd, du hade ingen refreshtoken :/");
+	}
+	try {
+		const decoded = jwt.verify(refreshToken, secret_key);
+		const accessToken = jwt.sign({ email: decoded.email }, secret_key, {
+			expiresIn: "15m",
+		});
+		res.status(200).header("Authorization", accessToken).send(decoded.email);
+	} catch (error) {
+		res.status(401).send("Neej va synd du får ingen ny token :'(");
+	}
 });
 
 export default router;
