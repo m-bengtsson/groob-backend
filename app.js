ee61aa12b497369ad817ab499c6a405b2d80239c
Kate <110845948+KateN87@users.noreply.github.com>
2023-10-19 15:02:05 +0200

    21 backend validate login (#22)
    * send user instead of email
    
    * middleware validate login

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 import express from "express";
 import cors from "cors";
 import cookies from "cookie-parser";
-import itemsRoute from "./routes/items.js"
-import usersRoute from "./routes/users.js"
-import identityRoute from "./routes/identity.js"
+import itemsRoute from "./routes/items.js";
+import usersRoute from "./routes/users.js";
+import identityRoute from "./routes/identity.js";
 
 const app = express();
 
 app.use(cookies());
 app.use(
-   cors({
-      origin: "http://localhost:5173",
-   })
+	cors({
+		origin: "http://localhost:5173",
+	})
 );
-app.use(express.json());
-app.use("/api/items", itemsRoute)
-app.use("/api/users", usersRoute)
-app.use("/api/identity", identityRoute)
 
+app.use(express.json());
+app.use("/api/items", itemsRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/identity", identityRoute);
 
 app.listen(8080, () => {
-   console.log("Server running on port 8080");
+	console.log("Server running on port 8080");
 });
---
+++ middleware/validate.js
@@ -0,0 +1,16 @@
+import { getUserByEmail } from "../database.js";
+
+export const validateLogin = async (req, res, next) => {
+	const { email, password } = req.body;
+	const maybeUser = await getUserByEmail(email);
+	if (!maybeUser) {
+		return res.status(401).send("Neeej, vad synd, du får inte logga in");
+	}
+
+	if (maybeUser.password !== password) {
+		return res.status(401).send("Neeej, vad synd, du får inte logga in");
+	}
+
+	req.user = maybeUser;
+	next();
+};
