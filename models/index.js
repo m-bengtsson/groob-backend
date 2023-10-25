52be08fdefbcd78ebcad87049b67a37773c7a7d0
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-10-25 14:58:31 +0200

    Add invitedUser table to db

--- models/index.js
+++ models/index.js
@@ -1,8 +1,9 @@
 import { Sequelize, DataTypes } from "sequelize";
 import config from "../database-config/connection.js";
-import userModel from "./user.model.js"
-import itemModel from "./item.model.js"
-import refreshTokenModel from "./refreshToken.model.js"
+import userModel from "./user.model.js";
+import itemModel from "./item.model.js";
+import refreshTokenModel from "./refreshToken.model.js";
+import invitedUserModel from "./invitedUser.model.js";
 
 const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
    host: config.HOST,
@@ -22,6 +23,7 @@ db.DataTypes = DataTypes;
 db.user = userModel(sequelize, DataTypes);
 db.item = itemModel(sequelize, DataTypes);
 db.refreshToken = refreshTokenModel(sequelize, DataTypes);
+db.invitedUser = invitedUserModel(sequelize, DataTypes);
 
 db.user.hasMany(db.item, {
    foreignKey: {
@@ -52,7 +54,6 @@ db.item.belongsTo(db.user, {
 });
 
 // Users to users
-
 db.user.hasMany(db.user, {
    foreignKey: {
       name: "createdBy",
@@ -77,12 +78,26 @@ db.user.hasMany(db.user, {
 db.user.belongsTo(db.user, {
    foreignKey: {
       name: "updatedBy",
+      foreignKeyConstraint: true
+   }
+});
+
+// InvitedUsers
+db.user.hasMany(db.invitedUser, {
+   foreignKey: {
+      name: "createdBy",
+      foreignKeyConstraint: true
+   }
+});
+
+db.invitedUser.belongsTo(db.user, {
+   foreignKey: {
+      name: "createdBy",
       foreignKeyConstraint: true
    }
 });
 
 // Refreshtoken
-
 db.refreshToken.belongsTo(db.user);
 
 export default db;
