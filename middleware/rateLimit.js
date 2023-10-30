36f90e9b93e871fac5654a1ce6d0c16a32a151c1
Kate <kate.nordqvist@gmail.com>
2023-10-30 16:14:20 +0100

    lock user when 9 failed login attempts

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -1,29 +1,36 @@
 import { rateLimit } from "express-rate-limit";
+import db from "../models/index.js";
 
 let failedAttempts = {};
 
+const User = db.user;
+
 export const setFailedAttempts = () => {
-  failedAttempts = {};
+	failedAttempts = {};
 };
 
-export const limiter = rateLimit({
-  windowMs: 10 * 1000,
-  max: (req) => {
-    const ip = req.ip;
+export const loginLimiter = rateLimit({
+	windowMs: 5 * 60 * 1000,
+	max: (req) => {
+		const ip = req.ip;
 
-    console.log("FAILED ATTEMPTS: ", failedAttempts);
-    if (failedAttempts[ip] >= 9) {
-      return 0;
-    }
-    return 3;
-  },
-  handler: (req, res, next) => {
-    const ip = req.ip;
-    failedAttempts[ip] = (failedAttempts[ip] || 0) + 1;
+		console.log("FAILED ATTEMPTS: ", failedAttempts);
+		if (failedAttempts[ip] >= 9) {
+			return 0;
+		}
+		return 3;
+	},
 
-    if (failedAttempts[ip] >= 9) {
-      return res.status(429).send("Reset password please");
-    }
-    return res.status(429).send("Too many requests, please try again later.");
-  },
+	handler: async (req, res, next) => {
+		const ip = req.ip;
+		const { email } = req.body;
+		failedAttempts[ip] = (failedAttempts[ip] || 0) + 1;
+
+		if (failedAttempts[ip] >= 9) {
+			await User.update({ isLocked: true }, { where: { email } });
+
+			return res.status(429).send("Reset password please");
+		}
+		return res.status(429).send("Too many requests, please try again later.");
+	},
 });
