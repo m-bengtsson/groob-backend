86dea18eb299c7e41fad126c89cc92fd46b7a9ea
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-11-02 14:52:39 +0100

    Update status code in authenticate

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -9,49 +9,49 @@ const secret_key_refresh = process.env.SECRET_KEY_REFRESH;
 const Refreshtoken = db.refreshToken;
 
 const authenticate = async (req, res, next) => {
-	const accessToken = req.headers["authorization"];
-	const refreshToken = req.cookies["refreshToken"];
+  const accessToken = req.headers["authorization"];
+  const refreshToken = req.cookies["refreshToken"];
 
-	if (!accessToken && !refreshToken) {
-		return res.status(401).send("Access Denied. No token provided.");
-	}
+  if (!accessToken && !refreshToken) {
+    return res.status(401).send("Access Denied. No token provided.");
+  }
 
-	try {
-		const decoded = jwt.verify(accessToken, secret_key_access);
-		req.user = decoded;
-		next();
-	} catch (error) {
-		if (!refreshToken) {
-			return res.status(401).send("Access Denied. No refresh token provided.");
-		}
+  try {
+    const decoded = jwt.verify(accessToken, secret_key_access);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    if (!refreshToken) {
+      return res.status(401).send("Access Denied. No refresh token provided.");
+    }
 
-		try {
-			const maybeTokenExists = await Refreshtoken.findOne({
-				where: { token: refreshToken },
-			});
+    try {
+      const maybeTokenExists = await Refreshtoken.findOne({
+        where: { token: refreshToken },
+      });
 
-			if (maybeTokenExists) {
-				const decoded = jwt.verify(refreshToken, secret_key_refresh);
+      if (maybeTokenExists) {
+        const decoded = jwt.verify(refreshToken, secret_key_refresh);
 
-				const user = await useGetUser(decoded.id);
+        const user = await useGetUser(decoded.id);
 
-				const accessToken = jwt.sign(user.dataValues, secret_key_access, {
-					expiresIn: "15m",
-				});
+        const accessToken = jwt.sign(user.dataValues, secret_key_access, {
+          expiresIn: "15m",
+        });
 
-				res
-					.cookie("refreshToken", refreshToken, {
-						httpOnly: true,
-						sameSite: "strict",
-					})
-					.header("Authorization", accessToken)
-					.send(user.dataValues);
-			}
-		} catch (error) {
-			console.log("BACKEND ERROR", error);
-			return res.status(400).send("Invalid Token.");
-		}
-	}
+        res
+          .cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            sameSite: "strict",
+          })
+          .header("Authorization", accessToken)
+          .send(user.dataValues);
+      }
+    } catch (error) {
+      console.log("BACKEND ERROR", error);
+      return res.status(401).send("Invalid Token.");
+    }
+  }
 };
 
 export default authenticate;
