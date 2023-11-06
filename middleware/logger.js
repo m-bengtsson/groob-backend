131f0f10af6c8747682941907d5f6a4e7a6e74c1
Kate <kate.nordqvist@gmail.com>
2023-11-06 15:19:27 +0100

    Add headers and cookies to logger middleware

--- middleware/logger.js
+++ middleware/logger.js
@@ -3,7 +3,9 @@ const logger = (req, res, next) => {
 	console.log("LOG Method: ", req.method);
 	console.log("LOG Url: ", req.url);
 	console.log("LOG IP:", req.ip);
-	console.log("LOG res status:", res.statusCode);
+	console.log("LOG Headers", req.headers);
+	console.log("LOG Cookies", req.cookies);
+
 	next();
 };
 
