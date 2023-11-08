fd9d8390df87988537463cf25ca086acea357ce2
Kate <kate.nordqvist@gmail.com>
2023-11-08 13:38:43 +0100

    Add link to reset password

--- hooks/useValidateRegEx.js
+++ hooks/useValidateRegEx.js
@@ -1,28 +1,31 @@
 export const usePassWordValidate = async (password, repeatPassword) => {
-  if (!password || !repeatPassword) {
-    throw new Error("All fields required");
-  }
+	if (!password || !repeatPassword) {
+		console.log("MISSING FIELD");
+		throw new Error("All fields required");
+	}
 
-  const strongPassword =
-    /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+	const strongPassword =
+		/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
-  if (!strongPassword.test(password)) {
-    throw new Error("Password is too weak");
-  }
+	if (!strongPassword.test(password)) {
+		console.log("TOO WEAK");
+		throw new Error("Password is too weak");
+	}
 
-  if (password !== repeatPassword) {
-    throw new Error("Passwords do not match");
-  }
+	if (password !== repeatPassword) {
+		console.log("DO NOT MATCH");
+		throw new Error("Passwords do not match");
+	}
 
-  return;
+	return;
 };
 
 export const useEmailValidate = (email) => {
-  const validEmail =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	const validEmail =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  if (!validEmail.test(email)) {
-    throw new Error("Invalid email");
-  }
-  return;
+	if (!validEmail.test(email)) {
+		throw new Error("Invalid email");
+	}
+	return;
 };
