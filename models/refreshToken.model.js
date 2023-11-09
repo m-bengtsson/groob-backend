9636e29571b0ce2612331b049919a617f8398f91
Kate <kate.nordqvist@gmail.com>
2023-11-09 15:48:19 +0100

    change refreshToken model
    Increased string to 500

--- models/refreshToken.model.js
+++ models/refreshToken.model.js
@@ -1,17 +1,17 @@
 const refreshTokenModel = (sequelize, DataTypes) => {
-   const RefreshToken = sequelize.define("refreshTokens", {
-      id: {
-         type: DataTypes.UUID,
-         primaryKey: true,
-         allowNull: false,
-         unique: true,
-      },
-      token: {
-         type: DataTypes.STRING(255),
-         allowNull: false,
-      },
-   })
-   return RefreshToken;
-}
+	const RefreshToken = sequelize.define("refreshTokens", {
+		id: {
+			type: DataTypes.UUID,
+			primaryKey: true,
+			allowNull: false,
+			unique: true,
+		},
+		token: {
+			type: DataTypes.STRING(500),
+			allowNull: false,
+		},
+	});
+	return RefreshToken;
+};
 
 export default refreshTokenModel;
