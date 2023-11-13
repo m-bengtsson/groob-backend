86794eaf2a211ccaa25e2bc0f56534b8734a37d3
Kate <110845948+KateN87@users.noreply.github.com>
2023-11-13 11:52:03 +0100

    add searchquery title (#68)
    - get query by query.title
    - if query is present, get item title by search query

--- hooks/useItem.js
+++ hooks/useItem.js
@@ -1,10 +1,25 @@
 import db from "../models/index.js";
 import { v4 as uuidv4 } from "uuid";
+import { Op } from "sequelize";
 
 const Item = db.item;
 
-export const useGetItems = async () => {
+//Op.like = case insensitive
+// % allow matching anything before and after the searched query
+export const useGetItems = async (searchQuery) => {
+	if (searchQuery) {
+		const foundItems = await Item.findAll({
+			where: {
+				title: {
+					[Op.like]: `%${searchQuery}%`,
+				},
+			},
+		});
+		return foundItems;
+	}
+
 	const foundItems = await Item.findAll();
+
 	return foundItems;
 };
 
