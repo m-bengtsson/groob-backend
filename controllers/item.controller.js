4237156a917bb79352a5d5c08bf12cb856d34133
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-11-13 13:17:32 +0100

    Return only title id, and description if user not authenticated

--- controllers/item.controller.js
+++ controllers/item.controller.js
@@ -1,86 +1,108 @@
 import {
-	useGetItems,
-	useGetItem,
-	useCreateItem,
-	useUpdateItem,
-	useDeleteItem,
+  useGetItems,
+  useGetItem,
+  useCreateItem,
+  useUpdateItem,
+  useDeleteItem,
 } from "../hooks/useItem.js";
+import db from "../models/index.js";
 
 export const getAllItems = async (req, res) => {
-	let searchQuery = req.query.title;
+  let searchQuery = req.query.title;
 
-	if (searchQuery) {
-		searchQuery = searchQuery.split("_").join(" ");
-	}
+  if (searchQuery) {
+    searchQuery = searchQuery.split("_").join(" ");
+  }
 
-	const items = await useGetItems(searchQuery);
-	if (!items) {
-		return res.status(500).send("Something went wrong, try again later");
-	}
-	res.status(200).send(items);
+  const items = await useGetItems(searchQuery);
+  if (!items) {
+    return res.status(500).send("Something went wrong, try again later");
+  }
+  res.status(200).send(items);
+  try {
+    const user = req.user;
+
+    if (!user) {
+      const publicItems = await db.item.findAll({
+        attributes: ["title", "id"],
+      });
+      return res.status(200).send(publicItems);
+    }
+
+    const items = await useGetItems();
+
+    if (!items) {
+      return res.status(500).send("Something went wrong, try again later");
+    }
+
+    res.status(200).send(items);
+  } catch (error) {
+    console.log("error: ", error);
+    res.status(500).send("Something went wrong, try again later");
+  }
 };
 
 export const getItem = async (req, res) => {
-	const id = req.params.id;
-	const item = await useGetItem(id);
+  const id = req.params.id;
+  const item = await useGetItem(id);
 
-	if (!item) {
-		return res.status(400).send("Could not find that item");
-	}
-	res.status(200).send(item);
+  if (!item) {
+    return res.status(400).send("Could not find that item");
+  }
+  res.status(200).send(item);
 };
 
 export const createItem = async (req, res) => {
-	const { title, description, numberOfItems } = req.body;
+  const { title, description, numberOfItems } = req.body;
 
-	const { id } = req.user;
-	const createdBy = id;
+  const { id } = req.user;
+  const createdBy = id;
 
-	if (!title || !description || !createdBy || !numberOfItems) {
-		return res.status(400).send("All fields required");
-	}
+  if (!title || !description || !createdBy || !numberOfItems) {
+    return res.status(400).send("All fields required");
+  }
 
-	try {
-		const created = await useCreateItem({
-			title,
-			description,
-			numberOfItems,
-			createdBy,
-		});
+  try {
+    const created = await useCreateItem({
+      title,
+      description,
+      numberOfItems,
+      createdBy,
+    });
 
-		res.status(201).send(created);
-	} catch (error) {
-		res.status(400).send("Something went wrong, try again later");
-	}
+    res.status(201).send(created);
+  } catch (error) {
+    res.status(400).send("Something went wrong, try again later");
+  }
 };
 
 export const updateItem = async (req, res) => {
-	const id = req.params.id;
-	const adminId = req.user.id;
-	const updated = await useUpdateItem({ ...req.body, updatedBy: adminId }, id);
-	try {
-		if (updated[0] === 0) {
-			return res.status(400).send("Something went wrong, try again later");
-		}
+  const id = req.params.id;
+  const adminId = req.user.id;
+  const updated = await useUpdateItem({ ...req.body, updatedBy: adminId }, id);
+  try {
+    if (updated[0] === 0) {
+      return res.status(400).send("Something went wrong, try again later");
+    }
 
-		const updatedItem = await useGetItem(id);
+    const updatedItem = await useGetItem(id);
 
-		res.status(201).send(updatedItem);
-	} catch (error) {
-		res.status(400).send("Something went wrong, try again later");
-	}
+    res.status(201).send(updatedItem);
+  } catch (error) {
+    res.status(400).send("Something went wrong, try again later");
+  }
 };
 
 export const deleteItem = async (req, res) => {
-	const id = req.params.id;
-	const item = await useDeleteItem(id);
-	try {
-		if (item === 0) {
-			return res.status(400).send("Something went wrong, try again later");
-		}
+  const id = req.params.id;
+  const item = await useDeleteItem(id);
+  try {
+    if (item === 0) {
+      return res.status(400).send("Something went wrong, try again later");
+    }
 
-		res.sendStatus(200);
-	} catch (error) {
-		res.status(400).send("Something went wrong, try again later");
-	}
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(400).send("Something went wrong, try again later");
+  }
 };
