f1bd137a56e5f43d490069021b5ea645b2dad8d5
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-11-15 13:44:49 +0100

    Fix eslint problems

--- database-config/database-init.js
+++ database-config/database-init.js
@@ -1,22 +1,17 @@
 import db from "../models/index.js";
-import bcrypt from "bcrypt";
 import mockUsers from "./mockUsers.json" assert { type: "json" };
 import mockItems from "./mockItems.json" assert { type: "json" };
 
-const saltRounds = 10;
-
 db.sequelize.sync({ force: true }).then(() => {
-	console.log("Drop and Resync Db");
-	initial();
+  console.log("Drop and Resync Db");
+  initial();
 });
-
-const hashedPassword = await bcrypt.hash("Password123!", saltRounds);
 
 const User = db.user;
 const Item = db.item;
 
 const initial = () => {
-	mockUsers.map((user) => User.create(user));
+  mockUsers.map((user) => User.create(user));
 
-	mockItems.map((item) => Item.create(item));
+  mockItems.map((item) => Item.create(item));
 };
